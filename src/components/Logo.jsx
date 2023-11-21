adfd982bbd6cbe35cba244fafec2b0a770287e98
Sujitgaikwad13 <sujitgaikwad121@gmail.com>
2023-11-21 21:34:16 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://tse4.mm.bing.net/th?id=OIP.VUhoNpaq8xQqDuWoT1cxogHaHa&pid=Api&P=0&h=180" className='w-10 h-10'  />
+    </div>
+  )
+}
+
+export default Logo
